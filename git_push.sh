6b40ec95572989317a98c2cc558ca0a2e1c1b8b2
Kathryn Scutt <kathryn.scutt@ttu.edu>
2021-11-10 11:11:03 -0600

    auto push

---
+++ git_push.sh
@@ -0,0 +1,12 @@
+
+cd Users/kathrynscutt/data
+git add .
+git commit -m"auto push"
+if [ -n "$(git status - porcelain)" ];
+then
+ echo "IT IS CLEAN"
+else
+ git status
+ echo "Pushing data to remote server!!!"
+ git push -u origin main
+fi

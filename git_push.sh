f0765b60c75b146993b7ea5ce2e6084aa34fc9a6
Kathryn Scutt <kathryn.scutt@ttu.edu>
2021-11-10 17:36:37 -0600

    data auto push

--- git_push.sh
+++ git_push.sh
@@ -1,6 +1,6 @@
 
 git pull.rebase false
-cd Users/kathrynscutt/Desktop/data
+cd Users/kathrynscutt/ProjLab3/TESTProject/Content/Data
 git add .
 git commit -m"data auto push"
 git push -u origin main

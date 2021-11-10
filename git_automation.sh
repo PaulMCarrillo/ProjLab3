479bc1ef5884d0e5f874a347b8e76da4cdf71a1f
davisokuz1 <davisokuzor@gmail.com>
2021-11-10 08:49:35 -0600

    1st commit

--- git_automation.sh
+++ git_automation.sh
@@ -1,4 +1,9 @@
-git add .
-git commit -m "this is a push x2"
-git push
+#!/bin/bash
+
+inotifywait -m /path -e create -e moved_to |
+    while read path action file; do
+        if [[ "$data" =~ .*txt$ ]]; then # Does the file end with .xml?
+            echo "txt file" # If so, do your thing here!
+        fi
+    done
 
